fbf8fc5d7d66197fab38e7b7534e17edc5589543
jpvila <jpvilanovas@gmail.com>
2021-05-26 14:59:49 -0300

    Update validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Waiting for 20 seconds before checking health.."
-sleep 20
+echo "Waiting for 120 seconds before checking health.."
+sleep 120
 
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:80)
 if [[ "$status_code" -ne 200 ]] ; then
